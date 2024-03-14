e42a15fa216ecd057cd232b41b0c80cb002e4ff2
Viemem <lcmem08@gmail.com>
2024-03-13 19:34:57 -0600

    a

---
+++ AI-Genrator/AI-Genrator/AI_GenratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_GenratorApp.swift
+//  AI-Genrator
+//
+//  Created by Guillermo Emiliano Landa Cerpa on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_GenratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
